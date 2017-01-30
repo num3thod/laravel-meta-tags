b2ac6a23a0b1217d4cbea9a6185875ed95128545
Josh Pine <joshpine@pmadigital.com>
2017-01-30 09:43:21 +0000

    Faking requests in tests.

--- tests/Tests.php
+++ tests/Tests.php
@@ -14,7 +14,7 @@ class Tests extends PHPUnit_Framework_TestCase
     {
         self::$title = self::text(20);
 
-        $this->Meta = new Meta(
+        $this->Meta = new Meta(null,
             [
                 'title_limit'       => 70,
                 'description_limit' => 200,
